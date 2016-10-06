232fcd9b187954a85ac48c16ec6f97d67a328348
marijnschilling <marijnschilling@gmail.com>
2016-10-06 11:52:13 +0200

    Create an instance of the view

--- Constraints.playground/Contents.swift
+++ Constraints.playground/Contents.swift
@@ -8,17 +8,20 @@ class ExpertiseView: UIView {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.systemFont(ofSize: 14)
         label.textColor = .gray
-        label.text = "tag"
 
         return label
     }()
 
-    init() {
+    init(title: String) {
         super.init(frame: CGRect.zero)
+
+        self.translatesAutoresizingMaskIntoConstraints = false
 
         self.backgroundColor = .blue
         self.layer.cornerRadius = 2
         self.layer.masksToBounds = true
+
+        self.titlelabel.text = "tag"
 
         self.addConstraints()
     }
@@ -34,7 +37,8 @@ class ExpertiseView: UIView {
 
         self.titlelabel.heightAnchor.constraint(equalToConstant: 17)
         self.titlelabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 0)
-        self.titlelabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        self.titlelabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10)
+        self.titlelabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10)
         self.titlelabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: margin)
         self.titlelabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -margin)
     }
@@ -44,7 +48,7 @@ class DeleteExpertiseView: ExpertiseView {
     lazy var deleteButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setImage(UIImage(named: "delete"), for: .normal)
+        button.setImage(UIImage(named: "delete")!, for: .normal)
 
         return button
 
@@ -58,6 +62,9 @@ class DeleteExpertiseView: ExpertiseView {
         self.deleteButton.heightAnchor.constraint(equalTo: self.heightAnchor)
         self.deleteButton.widthAnchor.constraint(equalTo: self.heightAnchor)
         self.deleteButton.topAnchor.constraint(equalTo: self.topAnchor)
+        self.deleteButton.leftAnchor.constraint(equalTo: self.titlelabel.rightAnchor)
         self.deleteButton.rightAnchor.constraint(equalTo: self.rightAnchor)
     }
 }
+
+let expertiseView = ExpertiseView(title: "tag")
